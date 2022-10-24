3b374b9c7cdc25bc90cc30a10849a0f3085fe496
Rodrigo Salmeron <rodd.sal23@gmail.com>
2022-10-24 02:18:52 -0500

    responsive cards

--- pages/trayectoria/index.tsx
+++ pages/trayectoria/index.tsx
@@ -1,4 +1,3 @@
-// Photos from https://citizenofnowhe.re/lines-of-the-city
 import { useRef } from "react";
 import {
   motion,
@@ -7,8 +6,8 @@ import {
   useTransform,
   MotionValue
 } from "framer-motion";
-import { Box, Button, Heading, SimpleGrid, Stack, Text } from "@chakra-ui/react";
-import Image from "next/image";
+import { Box, Button, Heading, Show, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+import Footer from '../../src/components/Footer';
 
 import {PROYECTOS} from './proyectos.js';
 
@@ -23,41 +22,56 @@ function ImageContainer({ id, title, date, desc, idx, total, img}) {
 
   return (
     <section>
-      <Box ref={ref} bg="white" w="50%">
-        <Box bg="brand.primary.800" w="100%" borderRadius="8px" overflow="hidden" boxShadow="lg" >
-          <Image
-            src={img}
-            alt={"CAPSULA"}
-            layout="fill"
-            objectFit='cover'
-          />
+      <SimpleGrid columns={[1, 1, 1, 2]} w="100%" mt="3rem" bg="white" >
+        <Box ref={ref} bg="white">
+          <Box
+            w='100%'
+            h='550px'
+            borderTopRadius='lg'
+            boxShadow="2xl"
+            p='1rem'
+            pos='relative'
+            bgImage={`url(${img.src})`}
+            bgSize='cover'
+            _before={{
+              content: '""',
+              bgColor: `brand.primary.800`,
+              bgSize: 'cover',
+              pos: 'absolute',
+              borderTopRadius: 'lg',
+              top: 0,
+              right: 0,
+              left: 0,
+              bottom: 0,
+              opacity: 0.5
+            }}/>
         </Box>
-      </Box>
-      <Box w="50%" p="0rem 2rem 0rem 3rem" bg="white" display="flex" alignItems="center" justifyContent="center" flexDir="column" >
-        <motion.h2 style={{ y }}>
-          <Stack direction='column' spacing={4} align='left'>
-            <Heading color="brand.primary.800" fontSize={"md"} >
-              {date}
-            </Heading>
-            <Heading color="brand.primary.800" fontSize={"5xl"} >
-              {title}
-            </Heading>
-            <Text fontSize={"sm"} color="brand.primary.800" >
-              {desc}
-            </Text>
-          </Stack>
-          <Button mt="1rem" colorScheme='brand.primary' color="white" size="md" shadow="lg" borderRadius="5em">
-            <Text cursor="pointer" p="0rem 1rem" fontSize={["xs", "sm", "sm", "sm"]}>
-              Leer mas
-            </Text>
-          </Button>
-        </motion.h2>
-      </Box>
+        <Box p="0rem 2rem 0rem 3rem" bg="white" display="flex" alignItems="center" justifyContent="center" flexDir="column" >
+          <motion.h2 style={{ y, marginTop: "-9rem" }}>
+            <Stack direction='column' spacing={4} align='left'>
+              <Heading color="brand.primary.800" fontSize={"md"} >
+                {date}
+              </Heading>
+              <Heading color="brand.primary.800" fontSize={"5xl"} >
+                {title}
+              </Heading>
+              <Text fontSize={"sm"} color="brand.primary.800" >
+                {desc}
+              </Text>
+            </Stack>
+            <Button mt="1rem" colorScheme='brand.primary' color="white" size="md" shadow="lg" borderRadius="5em">
+              <Text cursor="pointer" p="0rem 1rem" fontSize={["xs", "sm", "sm", "sm"]}>
+                Leer mas
+              </Text>
+            </Button>
+          </motion.h2>
+        </Box>
+      </SimpleGrid>
     </section>
   );
 }
 
-const Trayectoria = () => {
+const Trayectoria = ({ dictionary }) => {
   const { scrollYProgress } = useScroll();
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
@@ -65,14 +79,79 @@ const Trayectoria = () => {
     restDelta: 0.001
   });
 
+  const renderSingleCard = (item, i) => {
+    return(
+      <Box p="1rem" bg='white' boxShadow="lg" borderRadius="8px" display="flex" alignItems="center" justifyContent="center" flexDirection="column" key={i} >
+        <Box
+          w='100%'
+          h={["350px", "350px", "250px", "250px"]}
+          borderTopRadius='lg'
+          boxShadow="2xl"
+          p='1rem'
+          pos='relative'
+          bgImage={`url(${item?.img.src})`}
+          bgSize='cover'
+          _before={{
+            content: '""',
+            bgColor: `brand.primary.800`,
+            bgSize: 'cover',
+            pos: 'absolute',
+            borderTopRadius: 'lg',
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 0,
+            opacity: 0.5
+          }}
+        />
+        <Box pt="2rem" bg="white" display="flex" alignItems="center" justifyContent="center" flexDir="column" >
+          <Stack direction='column' spacing={4} align='left'>
+            <Heading color="brand.primary.800" fontSize={"xs"} >
+              {item.date}
+            </Heading>
+            <Heading color="brand.primary.800" fontSize={"3xl"} >
+              {item.title}
+            </Heading>
+            <Text fontSize={"sm"} color="brand.primary.800" >
+              {item.desc}
+            </Text>
+            <Button mt="1rem" colorScheme='brand.primary' color="white" size="md" shadow="lg" borderRadius="5em">
+            <Text cursor="pointer" p="0rem 1rem" fontSize={["sm", "sm", "sm", "sm"]}>
+              Leer mas
+            </Text>
+          </Button>
+          </Stack>
+        </Box>
+      </Box>
+    )
+  }
+
   return (
     <Box bg="white" padding={["0 4%", "0 4%", "0 4%", "0% 4%"]}>
-      {PROYECTOS.map((item, i) => (
-        <ImageContainer key={i} id={i} title={item.title} date={item.date} desc={item.desc} idx={i} total={PROYECTOS.length} img={item.img} />
-      ))}
-      <motion.div className="progress" style={{ scaleX }} />
+      <Show above="lg" >
+        {PROYECTOS.map((item, i) => (
+          <ImageContainer key={i} id={i} title={item.title} date={item.date} desc={item.desc} idx={i} total={PROYECTOS.length} img={item.img} />
+        ))}
+        <motion.div className="progress" style={{ scaleX }} />
+      </Show>
+      <Show below="lg" >
+        <SimpleGrid p={"6rem 0rem 2rem 0rem"} columns={[1, 1, 2, 2]} spacing='20px'>
+          {PROYECTOS.map((item, i) => (
+            renderSingleCard(item, i)
+          ))}
+        </SimpleGrid>
+      </Show>
     </Box>
   );
 }
 
 export default Trayectoria;
+
+export async function getStaticProps({locale}) {
+  const response = await import(`../../lang/${locale}.json`);
+  return {
+    props: {
+      dictionary: response.default
+    }
+  }
+}
--- pages/trayectoria/proyectos.js
+++ pages/trayectoria/proyectos.js
@@ -48,5 +48,47 @@ export const PROYECTOS = [
         date: "21-06-2018",
         img: XICO,
         desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        title: "Carretera Cuautzingo - Álamos Chalco",
+        date: "02-06-2018",
+        img: ALAMOS,
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        title: "Parque Tejones Valle de Chalco",
+        date: "05-06-2018",
+        img: TEJONES,
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        title: "Cerro del Sacromonte",
+        date: "09-06-2018",
+        img: SACROMONTE,
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        title: "Parque La Zapata Chalco",
+        date: "14-06-2018",
+        img: LAZAPATA,
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        title: "Vías del Ferrocarril Ayapango",
+        date: "16-06-2018 ",
+        img: AYAPANGO,
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        title: "Parque Héroes Chalco",
+        date: "20-01-2018",
+        img: HEROES,
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        title: "San Martín Xico Nuevo Chalco",
+        date: "21-06-2018",
+        img: XICO,
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     }
 ]
